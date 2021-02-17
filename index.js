7e5e9770a7da107e875c9f12416a9b103f3158bd
Olav van Schie <olav@appstract.nl>
2021-02-17 13:21:59 +0100

    export

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 
-import typeof useAxios from './hooks/useAxios';
-import typeof useMountedState from './hooks/useMountedState';
-import typeof useDebouncedState from './hooks/useDebouncedState';
+import useAxios from './hooks/useAxios';
+import useMountedState from './hooks/useMountedState';
+import useDebouncedState from './hooks/useDebouncedState';
 
 module.exports = {
-    get useAxios(): useAxios {
-        return require('./hooks/useAxios');
-    },
-    get useMountedState(): useMountedState {
-        return require('./hooks/useMountedState');
-    },
-    get useDebouncedState(): useDebouncedState {
-        return require('./hooks/useDebouncedState');
-    },
+    useAxios,
+    useMountedState,
+    useDebouncedState,
 }
